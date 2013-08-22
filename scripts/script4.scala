7b2c74caf6514c5c59c9ac5ac058e56e93c5c285
Mike Slinn <mslinn@micronauticsresearch.com>
2013-08-22 12:07:10 -0700

    -

--- scripts/script4.scala
+++ scripts/script4.scala
@@ -1,6 +1,6 @@
 class Frog1(val canSwim: Boolean, numLegs: Int, breathesAir: Boolean)
 object Frog1 {
-  def apply(val canSwim: Boolean=true, numLegs: Int=4, breathesAir: Boolean=true) = new Frog1(canSwim, numLegs, breathesAir)
+  def apply(canSwim: Boolean=true, numLegs: Int=4, breathesAir: Boolean=true) = new Frog1(canSwim, numLegs, breathesAir)
 }
 val frog1a = Frog1(canSwim=true)
 val frog1b = Frog1.apply(canSwim=true)
---
+++ scripts/script65.scala
@@ -0,0 +1,7 @@
+val x = 42
+x = 2
+var y = 42
+y = 3
+def echo(what: String): Unit = println(what)
+echo("Hi there!")
+
