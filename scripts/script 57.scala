6256a896e3ea8324f66af55a704c48244ac32d31
Mike Slinn <mslinn@micronauticsresearch.com>
2013-08-24 15:34:47 -0700

    -

---
+++ scripts/script 57.scala
@@ -0,0 +1,15 @@
+println("Hello, world!")
+val x = 3
+var y = 4
+y = 36
+def timesTwo(x: Int) = 2 * x
+timesTwo(21)
+1 to 3
+Range(1, 4)
+1 to 3 foreach { i => println("Hello, world!") }
+(1 to 3) foreach { i => println("Hello, world!") }
+(1 to 3).foreach { i => println("Hello, world!") }
+Range(1, 4).foreach { i => println("Hello, world!") }
+(1 to 3).foreach { i => println("Hello, world #" + i) }
+(1 to 3).foreach { i => println(s"Hello, world #$i") }
+
