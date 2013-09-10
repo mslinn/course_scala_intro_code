b42dabf81aa5d2cc6ff9e46793dfb35d8ecbaab3
Mike Slinn <mslinn@micronauticsresearch.com>
2013-09-10 05:10:35 -0700

    Updated after class

--- courseNotes/project/build.sbt
+++ courseNotes/project/build.sbt
@@ -1,7 +1,5 @@
-resolvers += "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
-
 resolvers += "Sonatype releases"  at "https://oss.sonatype.org/content/repositories/releases/"
 
-addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.2.0")
+//addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.2.0")
 
 addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.5.1")
---
+++ courseNotes/src/main/scala/WordSearch.scala
@@ -0,0 +1,43 @@
+/** Three solutions are shown. Solution 2 is best. */
+object WordSearch extends App {
+  val list = List("word", "another", "yet another")
+
+  type FunnyReturnType = Either[String, Int]
+
+  /** Efficient but clumsy */
+  def search(list: List[String], word: String): FunnyReturnType = {
+    val index = list.indexOf(word)
+    if (index<0)
+      Left(word)
+    else
+      Right(index)
+  }
+
+  /** Efficient, simple, elegant */
+  def search2(list: List[String], word: String): FunnyReturnType =
+    list.indexOf(word) match {
+      case -1 => Left(word)
+      case index => Right(index)
+  }
+
+  /** Overly complex, but shows some Scala constructs. Finds all matches and returns the first */
+  def search3(list: List[String], word: String): FunnyReturnType = {
+    val answers = for {
+      item <- list if item == word
+    } yield {
+      list.indexOf(item)
+    }
+    val result: FunnyReturnType =
+      answers.headOption match {
+        case Some(i) => Right(i)
+        case None => Left(word)
+      }
+    result
+  }
+
+  List("word", "oink", "blahbla", "another").foreach { w =>
+    println(s"Method 1 $w: ${search(list, w)}")
+    println(s"Method 2 $w: ${search2(list, w)}")
+    println(s"Method 3 $w: ${search3(list, w)}")
+  }
+}
