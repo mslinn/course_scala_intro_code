5eaa56a1eb0834cb2f022aa767f990f45e2e8702
Mike Slinn <mslinn@micronauticsresearch.com>
2013-12-30 11:39:27 -0800

    -

--- courseNotes/src/main/scala/TypeHierarchy.scala
+++ courseNotes/src/main/scala/TypeHierarchy.scala
@@ -34,6 +34,26 @@ object DogHog extends App {
   val dog = new Dog("Fido")
   val hog = new Hog("Porky")
 
-  println(s"Should a dog be compared to a hog? ${dog.canEqual(hog)}")
+  println(s"Should a dog be compared to a hog? ${dog.isInstanceOf[Hog]}")
   println(s"Comparing ${dog.name} with ${hog.name} gives: ${dog==hog}")
+}
+
+object MaybeDog2 extends App {
+  class Dog2(val name: String) {
+    override def equals(that: Any): Boolean = canEqual(that) && hashCode==that.hashCode
+
+    override def hashCode = name.hashCode
+
+    def canEqual(that: Any) : Boolean = that.isInstanceOf[Dog2]
+
+    def ===(that: Any): Boolean =
+      if (canEqual(that)) this==that else {
+        println(s"ERROR: ${getClass.getName} should not be compared to a ${that.getClass.getName}")
+        false
+      }
+  }
+
+  val dog2 = new Dog2("Fido2")
+  val maybeDog2 = Some(dog2)
+  println(s"Comparing dog2 with maybeDog2: ${dog2===maybeDog2}")
 }
