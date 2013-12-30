29f647641e64a7f00ea97e88a08708b4eb310f58
Mike Slinn <mslinn@micronauticsresearch.com>
2013-12-30 04:50:49 -0800

    -

--- courseNotes/src/main/scala/TypeHierarchy.scala
+++ courseNotes/src/main/scala/TypeHierarchy.scala
@@ -9,8 +9,31 @@ object TypeWidening1 extends App {
 
 object TypeWidening2 extends App {
   val rightNow = System.currentTimeMillis
-  val x: Any = if (rightNow % 2 == 0) true
-  else if (new java.util.Date(rightNow).toString.contains("Tue ")) "Today is a Tuesday"
-  else rightNow
+  val x: Any = if (rightNow % 2 == 0) true else
+  if (new java.util.Date(rightNow).toString.contains("Tue ")) "Today is a Tuesday" else rightNow
   println(s"x has type ${x.getClass.getName} and value $x")
+}
+
+object DogHog extends App {
+  class Dog(val name: String) {
+    override def equals(that: Any): Boolean = canEqual(that) && hashCode==that.hashCode
+
+    override def hashCode = name.hashCode
+
+    def canEqual(that: Any) : Boolean = that.isInstanceOf[Dog]
+  }
+
+  class Hog(val name: String) {
+    override def equals(that: Any): Boolean = canEqual(that) && hashCode==that.hashCode
+
+    override def hashCode = name.hashCode
+
+    def canEqual(that: Any) : Boolean = that.isInstanceOf[Hog]
+  }
+
+  val dog = new Dog("Fido")
+  val hog = new Hog("Porky")
+
+  println(s"Should a dog be compared to a hog? ${dog.canEqual(hog)}")
+  println(s"Comparing ${dog.name} with ${hog.name} gives: ${dog==hog}")
 }
