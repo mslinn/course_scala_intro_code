287681fab6dcb5a0fa0423a7d93dc1100d432b94
Mike Slinn <mslinn@micronauticsresearch.com>
2013-12-31 14:28:40 -0800

    -

--- courseNotes/src/main/scala/Animals.scala
+++ courseNotes/src/main/scala/Animals.scala
@@ -60,4 +60,35 @@ object Animals4 extends App {
   println(s"After assignment, frog3.numLegs=${frog3.numLegs}")
 
   // frog3.canSwim=false
+
+  val frog3b = new Frog3(canSwim=true, 4, breathesAir=true) { override def toString() = s"$canSwim; $numLegs legs" }
+  println(s"frog3b=$frog3b")
+}
+
+
+class Frog4(val canSwim: Boolean=true, var numLegs: Int=4, breathesAir: Boolean=true) extends Animal(numLegs, breathesAir) {
+  override def toString() = s"canSwim: $canSwim; $numLegs legs; breathesAir:$breathesAir"
+}
+
+object Animals5 extends App {
+  val frog4a = new Frog4
+  val frog4b = new Frog4(numLegs=2)
+  val frog4c = new Frog4(false)
+  val frog4d = new Frog4(false, breathesAir=false)
+
+  println(s"frog4a=$frog4a")
+  println(s"frog4b=$frog4b")
+  println(s"frog4c=$frog4c")
+  println(s"frog4d=$frog4d")
+}
+
+
+
+class Frog5(val canSwim: Boolean, var numLegs: Int, breathesAir: Boolean) extends Animal(numLegs, breathesAir) {
+  override def toString() = s"canSwim: $canSwim; $numLegs legs; breathesAir: $breathesAir"
+}
+
+object Animals6 extends App {
+   val frog5 = new Frog5(canSwim=true, 4, breathesAir=true)
+   println(s"frog5=$frog5")
 }
