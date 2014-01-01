6b947232ee0af7c7ac364382ff767a6b90e80dc7
Mike Slinn <mslinn@micronauticsresearch.com>
2014-01-01 15:15:37 -0800

    -

--- courseNotes/src/main/scala/VisibilityDemo.scala
+++ courseNotes/src/main/scala/VisibilityDemo.scala
@@ -139,21 +139,3 @@ object VisibleMain extends App {
   val publicInPublicInstance = new publicInstance.PublicInPublicClass
   println("All done!")
 }
-
-object SealedDemo extends App {
-  sealed abstract class Color
-  class White extends Color
-  class Yellow extends Color
-  class Blue extends Color
-  class Green extends Color
-  class Black extends Color
-
-  def colorSwatch(color: Color): Unit = color match {
-    case color: White => println("White")
-    case color: Yellow => println("Yellow")
-    case color: Blue => println("Blue")
-    case color: Green => println("Green")
-  } // compiler will complain that Black was not tested for
-
-  println(colorSwatch(new Blue))
-}
