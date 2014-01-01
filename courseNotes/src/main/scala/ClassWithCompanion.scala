adfc1069d053eed788b75f327f6a41a8f25dcfd1
Mike Slinn <mslinn@micronauticsresearch.com>
2014-01-01 17:18:32 +0000

    -

--- courseNotes/src/main/scala/ClassWithCompanion.scala
+++ courseNotes/src/main/scala/ClassWithCompanion.scala
@@ -21,6 +21,26 @@ object Animals7 extends App {
 }
 
 
+object Fractions extends App {
+  class Fraction(var numerator:Int, var denominator:Int) {
+    def *(fraction: Fraction) = Fraction(numerator*fraction.numerator, denominator*fraction.denominator)
+
+    override def toString = s"$numerator/$denominator"
+  }
+
+  object Fraction {
+    def apply(numerator: Int, denominator: Int) = new Fraction(numerator, denominator)
+
+    def unapply(fraction: Fraction) = if (fraction==null) None else Some(fraction.numerator, fraction.denominator)
+  }
+
+  val fraction = Fraction(3,4) * Fraction(2,4)
+  println(s"fraction=$fraction")
+  val Fraction(numer, denom) = Fraction(1, 4) * Fraction(4, 5)
+  println(s"numer=$numer, denom=$denom")
+}
+
+
 object Animals8 extends App {
   class Frog8(val canSwim: Boolean, val numLegs: Int, val breathesAir: Boolean) {
     override def toString() = s"canSwim: $canSwim; $numLegs legs; breathesAir: $breathesAir"
