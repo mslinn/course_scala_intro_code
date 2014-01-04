ea835b22dd9a2d10e92b2523ad6693d506d3ee8d
Mike Slinn <mslinn@micronauticsresearch.com>
2014-01-04 11:37:02 -0800

    -

--- courseNotes/src/main/scala/Auxilary.scala
+++ courseNotes/src/main/scala/Auxilary.scala
@@ -1,40 +1,25 @@
-object Auxilary1 extends App {
+object Auxilary extends App {
+  object Animal3 {
+    def apply(numLegs: Int, breathesAir: Boolean): Animal3 = new Animal3(numLegs, breathesAir)
+
+    def apply(): Animal3 = new Animal3 // method definition must have parentheses!
+  }
+
   class Animal3(numLegs: Int, breathesAir: Boolean) {
-    private val breatheMsg = if (breathesAir) "" else "do not"
-    val msg = s"I have $numLegs legs and I $breatheMsg breathe air"
+    private val breatheMsg = if (breathesAir) "" else " do not"
+    val msg = s"I have $numLegs legs and I$breatheMsg breathe air"
 
-    def this() = this(2, breathesAir=true)
+    def this() = this(numLegs=2, breathesAir=true) // you cannot define the return type
 
-    override def toString() = s"Animal3:msg=$msg"
+    override def toString() = s"numLegs$numLegs, breathesAir=$breathesAir, msg=$msg"
   }
 
-  val animal3 = new Animal3()
-  println(s"animal3.msg=${animal3.msg}")
-}
-
-
-object Auxilary2 extends App {
-  object Animal4 {
-    def apply(numLegs: Int, breathesAir: Boolean): Animal4 = new Animal4(numLegs, breathesAir)
-
-    def apply(): Animal4 = new Animal4 // method definition must have parentheses!
-  }
-
-  class Animal4(numLegs: Int, breathesAir: Boolean) {
-    private val breatheMsg = if (breathesAir) "" else "do not"
-    val msg = s"I have $numLegs legs and I $breatheMsg breathe air"
-
-    def this(): Animal4 = this(numLegs=2, breathesAir=true)
-
-    override def toString() = s"Animal4: numLegs$numLegs, breathesAir=$breathesAir, msg=$msg"
-  }
-
-  val animal4a = new Animal4(4, true)
-  val animal4b = Animal4(4, true)
-  val animal4c = new Animal4
-  val animal4d = Animal4() // without parentheses you get a reference to Animal4.apply, instead of invoking it
-  println(s"animal4a=$animal4a")
-  println(s"animal4b=$animal4b")
-  println(s"animal4c=$animal4c")
-  println(s"animal4d=$animal4d")
+  val animal3a = new Animal3(4, true)
+  val animal3b = Animal3(4, true)
+  val animal3c = new Animal3
+  val animal3d = Animal3() // without parentheses you get a reference to Animal3.apply, instead of invoking it
+  println(s"animal3a=$animal3a")
+  println(s"animal3b=$animal3b")
+  println(s"animal3c=$animal3c")
+  println(s"animal3d=$animal3d")
 }
