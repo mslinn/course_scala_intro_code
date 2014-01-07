8537b9bb54a4d0acfd2cc59ba17177ab96363b98
Mike Slinn <mslinn@micronauticsresearch.com>
2014-01-06 17:04:39 -0800

    -

--- courseNotes/src/main/scala/Traits.scala
+++ courseNotes/src/main/scala/Traits.scala
@@ -98,6 +98,42 @@ object BoldlyGo extends App {
 }
 
 
+object WhichTrait extends App {
+  trait Color {
+    def color: String = "Unknown"
+    def paint(highlight: String): String = s"$color with $highlight highlights"
+  }
+
+  trait Red extends Color {
+    abstract override val color: String = "red"
+    abstract override def paint(highlight: String): String = super.paint(s"pink and $highlight")
+  }
+
+  trait White extends Color {
+    abstract override val color: String = "white"
+    abstract override def paint(highlight: String): String = super.paint(s"light yellow and $highlight")
+  }
+
+  trait Blue extends Color {
+    abstract override val color: String = "blue"
+    abstract override def paint(highlight: String): String = super.paint(s"bluegreen and $highlight")
+  }
+
+  val red = new Red{}
+  println(s"""red.paint=${red.paint("polkadot")}""")
+
+  val redWhite = new Red with White
+  println(s"""redWhite.paint=${redWhite.paint("polkadot")}""")
+
+  val redWhiteBlue = new Red with White with Blue
+  println(s"""redWhiteBlue.paint=${redWhiteBlue.paint("polkadot")}""")
+
+  class Concrete extends Red with White with Blue
+  val concrete = new Concrete
+  println(s"""concrete.paint=${concrete.paint("polkadot")}""")
+}
+
+
 object ExtendJavaSet extends App {
   trait IgnoredCaseSet extends java.util.Set[String] {
     abstract override def add(str: String) = super.add(str.toLowerCase)
