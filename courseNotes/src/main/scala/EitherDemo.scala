da4b7036e2816ccbf6330280792573b2a011ffd5
Mike Slinn <mslinn@micronauticsresearch.com>
2014-01-09 08:27:05 -0800

    -

---
+++ courseNotes/src/main/scala/EitherDemo.scala
@@ -0,0 +1,37 @@
+object EitherDemo1 extends App {
+  val result1: Either[String, String] = Right("yay!")
+  println(s"result1.isRight=${result1.isRight}")
+  println(s"result1.isLeft=${result1.isLeft}")
+  println(s"result1.right.get=${result1.right.get}") // this is not the way to write production code!
+
+  val result2: Either[String, String] = Left("Oops, I did it again!")
+  println(s"result2.isRight=${result2.isRight}")
+  println(s"result2.isLeft=${result2.isLeft}")
+  println(s"result2.left.get=${result2.left.get}") // this is not the way to write production code!
+
+}
+
+
+object EitherDemo2 extends App {
+  type IntString = Either[String, Int]
+
+  def parse(in: String): IntString = try {
+    Right(in.toInt)
+  } catch {
+    case e: Exception =>
+      Left(in)
+  }
+
+  def show(either: IntString): Unit =
+    println(either match {
+      case Right(x) =>
+        s"Parsed Int: $x"
+
+      case Left(x) =>
+        s"Unparseable String: $x"
+    })
+
+  show(parse("1234"))
+  show(parse("12abc"))
+  show(parse("abc1234"))
+}
