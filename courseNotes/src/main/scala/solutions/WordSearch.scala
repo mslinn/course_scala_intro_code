123c2a85c4a675a377f338d0662337debfbedbf9
Mike Slinn <mslinn@micronauticsresearch.com>
2014-01-09 08:34:45 -0800

    -

--- courseNotes/src/main/scala/solutions/WordSearch.scala
+++ courseNotes/src/main/scala/solutions/WordSearch.scala
@@ -1,3 +1,5 @@
+package solutions
+
 /** Three solutions are shown. Solution 2 is best. */
 object WordSearch extends App {
   val list = List("word", "another", "yet another")
@@ -13,31 +15,18 @@ object WordSearch extends App {
       Right(index)
   }
 
-  /** Efficient, simple, elegant */
+  /** Efficient, simple, elegant.
+    * Here we see Scala's match keyword used as a multi-way branch.
+    * Each match is tried in order until one succeeds.
+    * In the second case, notice that the variable index is defined. It will match every value. */
   def search2(list: List[String], word: String): FunnyReturnType =
     list.indexOf(word) match {
       case -1 => Left(word)
       case index => Right(index)
   }
 
-  /** Overly complex, but shows some Scala constructs. Finds all matches and returns the first */
-  def search3(list: List[String], word: String): FunnyReturnType = {
-    val answers = for {
-      item <- list if item == word
-    } yield {
-      list.indexOf(item)
-    }
-    val result: FunnyReturnType =
-      answers.headOption match {
-        case Some(i) => Right(i)
-        case None => Left(word)
-      }
-    result
-  }
-
   List("word", "oink", "blahbla", "another").foreach { w =>
     println(s"Method 1 $w: ${search(list, w)}")
     println(s"Method 2 $w: ${search2(list, w)}")
-    println(s"Method 3 $w: ${search3(list, w)}")
   }
 }
