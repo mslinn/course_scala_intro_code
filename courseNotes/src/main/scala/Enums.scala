0e0e62a49df5bf17e19a3922c9dd544f1b459855
Mike Slinn <mslinn@micronauticsresearch.com>
2014-09-18 05:57:36 -0700

    -

--- courseNotes/src/main/scala/Enums.scala
+++ courseNotes/src/main/scala/Enums.scala
@@ -4,9 +4,9 @@ object JavaEnum extends App {
   if (day==Day.TUESDAY) println("Wimpy wants a hamburger")
   Day.valueOf("MONDAY")
 
-  def tellItLikeItIs(theDay: Day): Unit = { 
+  def tellItLikeItIs(theDay: Day): Unit = {
     val msg = theDay match {
-      case MONDAY => "Mondays are bad."          
+      case MONDAY => "Mondays are bad."
       case FRIDAY => "Fridays are better."
       case SATURDAY => "Weekends are best."
       case SUNDAY => "Weekends are best."
@@ -23,10 +23,10 @@ object JavaEnum extends App {
 object ScalaEnum extends App {
   object ScalaDay extends Enumeration {
     type ScalaDay = Value
-    val Monday    = Value("MONDAY") 
-    val Tuesday   = Value("TUESDAY") 
-    val Wednesday = Value("WEDNESDAY") 
-    val Thursday  = Value("THURSDAY") 
+    val Monday    = Value("MONDAY")
+    val Tuesday   = Value("TUESDAY")
+    val Wednesday = Value("WEDNESDAY")
+    val Thursday  = Value("THURSDAY")
     val Friday    = Value("FRIDAY")
     val Saturday  = Value("SATURDAY")
     val Sunday    = Value("SUNDAY")
@@ -34,10 +34,10 @@ object ScalaEnum extends App {
   val Mon = ScalaDay.withName("WEDNESDAY")
   val label = ScalaDay.Friday.toString
 
-  import ScalaDay._  
-  def tellItLikeItIs(theDay: ScalaDay): Unit = { 
+  import ScalaDay._
+  def tellItLikeItIs(theDay: ScalaDay): Unit = {
     val msg = theDay match {
-      case Monday => "Mondays are bad."          
+      case Monday => "Mondays are bad."
       case Friday => "Fridays are better."
       case Saturday => "Weekends are best."
       case Sunday => "Weekends are best."
@@ -54,19 +54,19 @@ object ScalaEnum extends App {
 object ScalaEnumCase extends App {
   sealed trait CaseDay
   object CaseDay {
-    case object Monday extends CaseDay;
-    case object Tuesday extends CaseDay; 
-    case object Wednesday extends CaseDay; 
-    case object Thursday extends CaseDay; 
-    case object Friday extends CaseDay; 
-    case object Saturday extends CaseDay; 
-    case object Sunday extends CaseDay; 
+    case object Monday extends CaseDay
+    case object Tuesday extends CaseDay
+    case object Wednesday extends CaseDay
+    case object Thursday extends CaseDay
+    case object Friday extends CaseDay
+    case object Saturday extends CaseDay
+    case object Sunday extends CaseDay
   }
-  
+
   import CaseDay._
-  def tellItLikeItIs(theDay: CaseDay): Unit = { 
+  def tellItLikeItIs(theDay: CaseDay): Unit = {
     val msg = theDay match {
-      case Monday => "Mondays are bad."          
+      case Monday => "Mondays are bad."
       case Friday => "Fridays are better."
       case Saturday => "Weekends are best."
       case Sunday => "Weekends are best."
