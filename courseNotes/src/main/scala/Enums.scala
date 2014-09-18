d0c67b137a8e527ff1207502e5b701610a02e73d
Mike Slinn <mslinn@micronauticsresearch.com>
2014-09-18 06:56:00 -0700

    Added BlendedEnums

--- courseNotes/src/main/scala/Enums.scala
+++ courseNotes/src/main/scala/Enums.scala
@@ -49,11 +49,11 @@ object ScalaEnum extends App {
   tellItLikeItIs(Tuesday)
   tellItLikeItIs(Wednesday)
   tellItLikeItIs(Sunday)
-  
+
   object Colors extends Enumeration {
    val Red, Blue, Green = Value
   }
- 
+
   object Methods {
     //def myMethod(x: Colors.Value)  = "That's a color"
     //def myMethod(x: ScalaDay.Value) = "That's a weekday"
@@ -79,6 +79,70 @@ object ScalaEnumCase extends App {
       case Friday => "Fridays are better."
       case Saturday => "Weekends are best."
       case Sunday => "Weekends are best."
+      case _ => "Midweek days are so-so."
+    }
+    println(msg)
+  }
+  tellItLikeItIs(Monday)
+  tellItLikeItIs(Tuesday)
+  tellItLikeItIs(Wednesday)
+  tellItLikeItIs(Sunday)
+}
+
+object BlendedEnum extends App {
+  import Day._
+
+  trait EnumLike[T] {
+    def name: String
+
+    def compareTo(other: Day): Int
+
+    override def equals(other: Object): Boolean = other.equals()
+
+    def hashCode: Int
+
+    def ordinal: Int
+
+    def toString: String
+
+    def values: Array[T]
+  }
+
+  sealed class CaseDay(val day: Day) extends EnumLike[CaseDay] {
+    import CaseDay._
+
+    lazy val name: String = day.name
+
+    def compareTo(other: Day): Int = day.compareTo(other)
+
+    override lazy val hashCode: Int = day.hashCode
+
+    lazy val ordinal: Int = day.ordinal
+
+    override lazy val toString = day.toString
+
+    lazy val values: Array[CaseDay] = Array(Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday)
+  }
+
+  object CaseDay {
+    case object Monday extends CaseDay(MONDAY)
+    case object Tuesday extends CaseDay(TUESDAY)
+    case object Wednesday extends CaseDay(WEDNESDAY)
+    case object Thursday extends CaseDay(THURSDAY)
+    case object Friday extends CaseDay(FRIDAY)
+    case object Saturday extends CaseDay(SATURDAY)
+    case object Sunday extends CaseDay(SUNDAY)
+
+    def valueOf(name: String) = Day.valueOf(name)
+  }
+
+  import CaseDay._
+  def tellItLikeItIs(theDay: CaseDay): Unit = {
+    val msg = theDay.ordinal match {
+      case Monday.ordinal => "Mondays are bad."
+      case Friday.ordinal => "Fridays are better."
+      case Saturday.ordinal => "Weekends are best."
+      case Sunday.ordinal => "Weekends are best."
       case _ => "Midweek days are so-so."
     }
     println(msg)
