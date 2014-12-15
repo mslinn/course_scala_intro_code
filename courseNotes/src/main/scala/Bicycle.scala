17779cb0666ba1e4db4f8c626b8761d05d2554f7
Mike Slinn <mslinn@micronauticsresearch.com>
2014-12-14 16:36:42 -0800

    Using Lists instead of Arrays for the Bicycle

--- courseNotes/src/main/scala/Bicycle.scala
+++ courseNotes/src/main/scala/Bicycle.scala
@@ -1,15 +1,15 @@
 object Bicycle extends App {
   trait Cassette {
     /** The number of cogs on each gear in the cassette */
-    protected def rearTeeth: Array[Int]
+    protected def rearTeeth: List[Int]
   }
 
   trait TouringCassette extends Cassette {
-    protected val rearTeeth = Array(14, 16, 18, 20, 22, 24, 26)
+    protected val rearTeeth = List(14, 16, 18, 20, 22, 24, 26)
   }
 
   trait RacingCassette extends Cassette {
-    protected val rearTeeth = Array(11, 12, 13, 14, 15, 17, 19, 21, 23, 25)
+    protected val rearTeeth = List(11, 12, 13, 14, 15, 17, 19, 21, 23, 25)
   }
 
   trait Tire {
