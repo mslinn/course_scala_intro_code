726e05f3b8080909f545bd37f1e2c9a168bee659
Mike Slinn <mslinn@micronauticsresearch.com>
2014-12-31 12:48:36 -0800

    Moved Anonymous class examples to Animals.scala

--- courseNotes/src/main/scala/Animals/Animals.scala
+++ courseNotes/src/main/scala/Animals/Animals.scala
@@ -134,3 +134,32 @@ object VarArgsScala extends App {
   val square = polygonFrom(new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(1, 0))
   println(s"Bounding box of polygon is: ${square.getBounds2D}")
 }
+
+object Anonymous1 extends App {
+  val adhoc = new {
+    def test(predicate: Boolean): String = if (predicate) "Yes" else "No"
+  }
+
+  println(s"adhoc.test(3>2)=${adhoc.test(3>2)}")
+}
+
+object Anonymous2 extends App {
+  import java.util.Date
+
+  val myDate = new Date { def dayAfter = new Date(getTime + 1000L*60L*60L*24L) }
+  println(s"myDate.dayAfter=${myDate.dayAfter}")
+}
+
+object Anonymous3 extends App {
+  abstract class A {
+    val a: Int
+    def doubleA: Int
+  }
+
+  val a = new A {
+    val a = 3
+    def doubleA = a * 2
+  }
+
+  println(s"a.doubleA=${a.doubleA}")
+}
