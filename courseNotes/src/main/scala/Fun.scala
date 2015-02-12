fffe6302bf294ef0aff7da7906dc62c0b31aed18
Mike Slinn <mslinn@micronauticsresearch.com>
2015-02-11 16:23:23 -0800

    Synced with lecture

--- courseNotes/src/main/scala/Fun.scala
+++ courseNotes/src/main/scala/Fun.scala
@@ -1,45 +1,3 @@
-object LazyEval extends App {
-  import java.util.Calendar
-
-  /** The Leibniz series algorithm converges slowly to Pi.
-    * About 5 billion iterations are required to yield accuracy to 10 decimal places.
-    * Pi is the limit of this series: 4/1 - 4/3 + 4/5 - 4/7 + 4/9 ...
-    * @see http://en.wikipedia.org/wiki/Leibniz_formula_for_%CF%80 */
-  def leibnizPi(iterationCount: Long=5000000000L, digits: Int=10): BigDecimal = {
-    val numerator: BigDecimal = 4.0
-    var denominator: BigDecimal = 1
-    var plus = true
-    var result: BigDecimal = 0.0
-    while (denominator < iterationCount) {
-      if (plus) {
-        result = result + numerator / denominator
-        plus = false
-      } else {
-        result = result - numerator / denominator
-        plus = true
-      }
-      denominator = denominator + 2
-    }
-    result.setScale(digits, BigDecimal.RoundingMode.HALF_UP)
-  }
-
-  val isWitchingHour: Boolean = Calendar.getInstance().get(Calendar.HOUR_OF_DAY) == 0
-  val scaredMsg = "I am too scared to compute"
-
-  def timidPi1(value: BigDecimal): String       =
-    if (!isWitchingHour) s"Eager evaluation yields $value"        else scaredMsg
-
-  def timidPi2(value: => BigDecimal): String    =
-    if (!isWitchingHour) s"Lazy evaluation yields $value"         else scaredMsg
-
-  def timidPi3(value: () => BigDecimal): String =
-    if (!isWitchingHour) s"Evaluating function yields ${value()}" else scaredMsg
-
-  println(timidPi1(leibnizPi()))        // eager evaluation
-  println(timidPi2(leibnizPi()))        // lazy evaluation
-  println(timidPi3(() => leibnizPi()))  // no-arg function
-}
-
 object Fun extends App {
   type IntDblToStr = (Int, Double) => String
 
@@ -117,12 +75,52 @@ object Closures extends App {
   Outer.Inner
 }
 
+object LazyEval extends App {
+  import java.util.Calendar
+
+  /** The Leibniz series algorithm converges slowly to Pi.
+    * About 5 billion iterations are required to yield accuracy to 10 decimal places.
+    * Pi is the limit of this series: 4/1 - 4/3 + 4/5 - 4/7 + 4/9 ...
+    * @see http://en.wikipedia.org/wiki/Leibniz_formula_for_%CF%80 */
+  def leibnizPi(iterationCount: Long=5000000000L, digits: Int=10): BigDecimal = {
+    val numerator: BigDecimal = 4.0
+    var denominator: BigDecimal = 1
+    var plus = true
+    var result: BigDecimal = 0.0
+    while (denominator < iterationCount) {
+      if (plus) {
+        result = result + numerator / denominator
+        plus = false
+      } else {
+        result = result - numerator / denominator
+        plus = true
+      }
+      denominator = denominator + 2
+    }
+    result.setScale(digits, BigDecimal.RoundingMode.HALF_UP)
+  }
+
+  val isWitchingHour: Boolean = Calendar.getInstance().get(Calendar.HOUR_OF_DAY) == 0
+  val scaredMsg = "I am too scared to compute"
+
+  def timidPi1(value: BigDecimal): String       =
+    if (!isWitchingHour) s"Eager evaluation yields $value"        else scaredMsg
+
+  def timidPi2(value: => BigDecimal): String    =
+    if (!isWitchingHour) s"Lazy evaluation yields $value"         else scaredMsg
+
+  def timidPi3(value: () => BigDecimal): String =
+    if (!isWitchingHour) s"Evaluating function yields ${value()}" else scaredMsg
+
+  println(timidPi1(leibnizPi()))        // eager evaluation
+  println(timidPi2(leibnizPi()))        // lazy evaluation
+  println(timidPi3(() => leibnizPi()))  // no-arg function
+}
+
 object MultipleParamLists extends App {
   import scala.annotation.tailrec
 
-  def unless(cond: => Boolean)(body: => Unit): Unit = {
-    if (!cond) body
-  }
+  def unless(cond: Boolean)(body: => Unit): Unit = if (!cond) body
 
   @tailrec
   def until(body: => Unit)(cond: => Boolean): Unit = {
@@ -134,13 +132,6 @@ object MultipleParamLists extends App {
   unless(x == 0) {println(s"I can divide by x because x is not zero: ${3 / x}")}
   x = 0
   unless(x == 0) {println(s"I can divide by x because x is not zero: ${3 / x}")}
-
-  val guardAgainstZero = unless(x == 0) _
-  x = 1
-  guardAgainstZero {println(s"I can divide by x because I'm guarded: ${3 / x}")}
-  x = 0
-  guardAgainstZero {println(s"I can divide by x because I'm guarded: ${3 / x}")}
-
 
   var i = 0
   until {
