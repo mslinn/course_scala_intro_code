b80b505f10408f8530584a6db38800dea2d6edfc
Mike Slinn <mslinn@mslinn.com>
2016-01-27 12:40:03 -0800

    -

--- courseNotes/src/test/scala/ScalaTestDemo.scala
+++ courseNotes/src/test/scala/ScalaTestDemo.scala
@@ -5,16 +5,16 @@ import org.scalatest._
 @RunWith(classOf[JUnitRunner])
 class ScalaTestDemo extends WordSpec {
 
-  """The "Hello world" string""" should {
+  "The 'Hello world' string" should {
     "contain 11 characters" in {
       assert("Hello world".length == 11)
     }
 
-    """start with "Hello"""" in {
+    "start with 'Hello'" in {
       assert("Hello world".startsWith("Hello"))
     }
 
-    """end with "world"""" in {
+    "end with 'world'" in {
       assert("Hello world".endsWith("world"))
     }
   }
