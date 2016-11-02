b28095cc311ebb007ccae0cebeda2e6fe6be64fe
Mike Slinn <mslinn@micronauticsresearch.com>
2016-11-02 10:09:56 -0700

    Updated to Scala 2.12

--- courseNotes/src/test/scala/solutions/ScalaTestSolution.scala
+++ courseNotes/src/test/scala/solutions/ScalaTestSolution.scala
@@ -6,7 +6,7 @@ class ScalaTestSolution extends WordSpec {
 
   "ScalaCourses.com" should {
     "contain the word scala" in {
-      val contents = io.Source.fromURL("http://scalacourses.com").getLines().mkString
+      val contents = io.Source.fromURL("http://scalacourses.com").getLines.mkString
       contents.toLowerCase.contains("scala")
     }
   }
--- courseNotes/src/test/scala/solutions/Specs2Solution.scala
+++ courseNotes/src/test/scala/solutions/Specs2Solution.scala
@@ -5,7 +5,7 @@ import org.specs2.mutable._
 class Specs2Solution extends Specification {
   "ScalaCourses.com" should {
     "contain the word scala" in {
-      val contents = io.Source.fromURL("http://scalacourses.com").getLines().mkString
+      val contents = io.Source.fromURL("http://scalacourses.com").getLines.mkString
       contents.toLowerCase must contain("scala")
     }
   }
