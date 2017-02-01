533f7511c2265e93d8b367483a0c0e9dca615a64
Mike Slinn <mslinn@micronauticsresearch.com>
2017-02-01 10:41:54 -0800

    -

--- courseNotes/src/test/scala/TestScalaTest.scala
+++ courseNotes/src/test/scala/TestScalaTest.scala
@@ -4,7 +4,7 @@ import org.scalatest._
 import org.scalatest.Matchers._
 
 @RunWith(classOf[JUnitRunner])
-class ScalaTestDemo extends WordSpec {
+class TestScalaTest extends WordSpec {
   "The 'Hello world' string" should {
     "contain 11 characters" in {
       "Hello world".length === 11
